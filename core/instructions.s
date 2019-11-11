5e6f264f23f61f99b3dd30301c7e0b2cb3c9f4d2
Daniel dos Santos Pereira <danield1591998@gmail.com>
2019-11-11 19:47:32 -0300

    Addu works with extended signal as well

--- core/instructions.s
+++ core/instructions.s
@@ -15,23 +15,7 @@ execute_addiu:
     la		$t1, IR_campo_imm   # $t1 <- &IR_campo_imm
     lw		$t1, 0($t1)		    # $t1 <- Valor de IR_campo_imm - Aqui ja temos o valor imediato
    
-    andi    $t3, $t1, check_signal_mask # Pegamos o MSB com essa mascara
-    beqz    $t3, imm_eh_positivo    # if $t3 == 0 then imm_eh_positivo
-    j       imm_eh_negativo         # else imm_eh_negativo
-
-    # Agora faremos a operacao (addu ou subu) de $t0 e $t1 e salvaremos em $t2 para depois colocar em $rt
-    imm_eh_positivo:
-    addu    $t2, $t0, $t1       # Usei o addu porque estamos simulando o addiu, mas aqui o nosso imm esta em uma variavel
-    j		epilogo_operacao	# jump to epilogo_operacao
-    
-    imm_eh_negativo:
-    # Quer dizer que o nosso imm esta com extensao de sinal e eh negativo
-    # portanto precisamos inverter e somar 1 para obter o valor positivo
-    # e depois fazer a subtracao do mesmo
-    xori    $t1, $t1, invert_mask   # Invertemos os bits
-    addi	$t1, $t1, 1             # Somamos 1
-    subu    $t2, $t0, $t1
-    j		epilogo_operacao	    # jump to epilogo_operacao
+    addu    $t2, $t0, $t1
 
     epilogo_operacao:
     la		$t0, IR_campo_rt    # $t0 <- &IR_campo_rt
